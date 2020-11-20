4c9dfabe4f9087beda109cdad76ac14ced71af19
berhanbezualem <berhanutegebaru@gmail.com>
2020-11-19 23:03:36 -0500

    test

---
+++ src/stringapps/Stringapps.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringapps;
+
+/**
+ *
+ * @author Berhanu
+ */
+public class Stringapps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String str="berhanu";
+        
+        System.out.println(str.substring(3, 4));
+        
+    }
+    
+}
